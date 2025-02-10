397028079c72dc77eca12f5a9163d4fac9e515e3
Srivatsan <talkads123@gmail.com>
2025-02-10 21:55:19 +0000

    Create Task and TaskManager objects

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,31 @@
+#include "TaskManager.hpp"
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+  Task task1(1, "Task 1", false);
+  Task task2(2, "Task 2", true);
+  Task task3(3, "Task 3", false);
+  // Test task getters
+  std::cout << "Task 1 ID: " << task1.getId() << std::endl;
+  std::cout << "Task 1 Title: " << task1.getTitle() << std::endl;
+  std::cout << "Task 1 Completed: "
+            << (task1.getIsCompleted() ? "True" : "False") << std::endl;
+  // Test task setters
+  task1.setId(4);
+  task1.setTitle("Task 4");
+  task1.setIsCompleted(true);
+  // Test task setters worked
+  std::cout << "Task 1 ID: " << task1.getId() << std::endl;
+  std::cout << "Task 1 Title: " << task1.getTitle() << std::endl;
+  std::cout << "Task 1 Completed: "
+            << (task1.getIsCompleted() ? "True" : "False") << std::endl;
+
+  // Test TaskManager class
+  TaskManager taskManager1;
+  taskManager1.addTask(task1);
+  taskManager1.addTask(task2);
+  taskManager1.addTask(task3);
+  taskManager1.displayTasks();
+
+  std::cout << "Hello World!\n";
+}
